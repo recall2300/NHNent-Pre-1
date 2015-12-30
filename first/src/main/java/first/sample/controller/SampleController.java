d8ed75682919cbde090a24f58f827ddeb8982fd2
recall2300 <recall2300@gmail.com>
2015-12-30 20:55:35 +0900

    submit

--- first/src/main/java/first/sample/controller/SampleController.java
+++ first/src/main/java/first/sample/controller/SampleController.java
@@ -1,7 +1,9 @@
 package first.sample.controller;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.annotation.Resource;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import first.common.common.CommandMap;
 import first.sample.service.SampleService;
 
 @Controller
@@ -19,12 +22,34 @@ public class SampleController {
     @Resource(name="sampleService")
     private SampleService sampleService;
      
-    @RequestMapping(value="/sample/openSampleBoardList.do")
+    @RequestMapping(value="/sample/openBoardList.do")
     public ModelAndView openSampleBoardList(Map<String,Object> commandMap) throws Exception{
         ModelAndView mv = new ModelAndView("/sample/boardList");
          
         List<Map<String,Object>> list = sampleService.selectBoardList(commandMap);
         mv.addObject("list", list);
+         
+        return mv;
+    }
+    
+    @RequestMapping(value="/sample/testMapArgumentResolver.do")
+    public ModelAndView testMapArgumentResolver(CommandMap commandMap) throws Exception{
+        ModelAndView mv = new ModelAndView("");
+         
+        if(commandMap.isEmpty() == false){
+            Iterator<Entry<String,Object>> iterator = commandMap.getMap().entrySet().iterator();
+            Entry<String,Object> entry = null;
+            while(iterator.hasNext()){
+                entry = iterator.next();
+                log.debug("key : "+entry.getKey()+", value : "+entry.getValue());
+            }
+        }
+        return mv;
+    }
+    
+    @RequestMapping(value="/sample/openBoardWrite.do")
+    public ModelAndView openBoardWrite(CommandMap commandMap) throws Exception{
+        ModelAndView mv = new ModelAndView("/sample/boardWrite");
          
         return mv;
     }
